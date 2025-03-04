0a7f3c335c64c2f2423afd93cdc14661fc8c3ba2
M9heb <123528241+M9heb@users.noreply.github.com>
2025-03-04 14:53:49 +0430

    Adds quiz and questions screen pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import "package:flutter/material.dart";
-import "package:quiz_app/start_screen.dart";
+import "package:quiz_app/quiz.dart";
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 188, 108, 37),
-              Color.fromARGB(255, 221, 161, 94)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import "package:quiz_app/start_screen.dart";
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 188, 108, 37),
+                Color.fromARGB(255, 221, 161, 94)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
