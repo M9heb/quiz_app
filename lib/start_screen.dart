fa799548c4b4e7562a61056fd1bc34d6c0e32f82
M9heb <123528241+M9heb@users.noreply.github.com>
2025-03-04 13:48:57 +0430

    Changes the styles of outlined button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -11,6 +11,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             "assets/images/quiz-logo.png",
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
           const SizedBox(
             height: 80,
@@ -21,11 +22,19 @@ class StartScreen extends StatelessWidget {
                 color: Color.fromARGB(217, 255, 255, 255), fontSize: 24),
           ),
           const SizedBox(height: 24),
-          OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                  foregroundColor: const Color.fromARGB(255, 254, 250, 224)),
-              onPressed: () {},
-              child: Text("Start the Quiz!"))
+          OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: const Color.fromARGB(255, 254, 250, 224),
+              side: BorderSide(color: const Color.fromARGB(150, 255, 255, 255)),
+            ),
+            onPressed: () {},
+            label: Text("Start the Quiz!"),
+            icon: Icon(
+              Icons.arrow_right_alt,
+              color: Colors.white,
+            ),
+            iconAlignment: IconAlignment.end,
+          )
         ],
       ),
     );
